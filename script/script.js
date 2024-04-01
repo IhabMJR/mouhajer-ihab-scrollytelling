4737da26cfd38e09f147e8eb9259b508f57ff7ed
RandomizedRaptor <i.mseriousstuff@gmail.com>
2024-04-01 13:33:33 -0400

    partie header fini

--- script/script.js
+++ script/script.js
@@ -1,5 +1,15 @@
- // use a script tag or an external JS file
- document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(ScrollTrigger)
-    // gsap code here!
-   });
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.fromTo(
+    ".cta",
+    { bottom: "8vw" },
+    {
+      bottom: "3vw",
+      duration: 0.5,
+      repeat: -1,
+      "transition-timing-function": "linear",
+      yoyo: true,
+      ease: "expo.in",
+    }
+  );
+});
